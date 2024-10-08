ec14434e32bcb28d768004b37e88e23355b117af
ufsurvey <36179053+ufsurvey@users.noreply.github.com>
2024-10-07 22:37:48 -0400

    Update econet.cpp

--- components/econet/econet.cpp
+++ components/econet/econet.cpp
@@ -8,7 +8,7 @@ namespace econet {
 static const char *const TAG = "econet";
 
 static const uint32_t RECEIVE_TIMEOUT = 200;
-static const uint32_t REQUEST_DELAY = 2000;
+static const uint32_t REQUEST_DELAY = 10000;
 
 static const uint8_t DST_ADR_POS = 0;
 static const uint8_t SRC_ADR_POS = 5;
